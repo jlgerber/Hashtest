3cd58ad6c47b444d0eba22c2db0a1abebb6f0db8
jlgerber <jlgerber@gmail.com>
2020-09-07 01:46:50 -0700

    adds tests

--- src/unit_tests/hashit_test.rs
+++ src/unit_tests/hashit_test.rs
@@ -1,4 +1,24 @@
 use super::*;
-
+use crate::string::{HtString, StringHash};
 #[test]
-fn 
+fn has_changed_given_new_items_is_true() {
+    let input = "/this/is/new";
+    let output = "/foo/bar";
+    let mut hashit = Hashit {
+        inner: HtString::new(),
+        hasher: StringHash {},
+    };
+    let has_changed = hashit.has_changed(&vec![input][..], output);
+    assert!(has_changed.unwrap());
+}
+#[test]
+fn has_changed_given_existing_items_is_false() {
+    let input = "/this/is/new";
+    let output = "/this/is/new";
+    let mut hashit = Hashit {
+        inner: HtString::new(),
+        hasher: StringHash {},
+    };
+    let has_changed = hashit.has_changed(&vec![input][..], output);
+    assert_eq!(has_changed.unwrap(), false);
+}
